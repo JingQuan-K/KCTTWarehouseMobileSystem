25ae6a07be68010540304cc064669d9d275d2313
KJ <kangjing324@live.com>
2021-03-28 17:39:05 +0800

    From KJ

--- app/src/main/java/com/example/kcttwarehousemobilesystem/MainActivity.kt
+++ app/src/main/java/com/example/kcttwarehousemobilesystem/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 //hi this is a comment by kjq
+//pepek busuk
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
