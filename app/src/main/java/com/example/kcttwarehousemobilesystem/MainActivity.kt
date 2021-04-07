3162f93e5452c49c7796b4379da5619b250e1e78
HENRYTZL <henrytzl2@hotmail.com>
2021-04-07 11:10:03 +0800

    Register

--- app/src/main/java/com/example/kcttwarehousemobilesystem/MainActivity.kt
+++ app/src/main/java/com/example/kcttwarehousemobilesystem/MainActivity.kt
@@ -3,14 +3,6 @@ package com.example.kcttwarehousemobilesystem
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-//hi this is a comment by kjq
-//pepek busuk haha
-//sohai 7 sohai
-
-//jibai
-
-//henrytzl
-//diulie lou mei macao hai
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
