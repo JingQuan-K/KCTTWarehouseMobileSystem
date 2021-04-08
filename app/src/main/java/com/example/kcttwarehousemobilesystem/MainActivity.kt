9bace32fbd428874534aee637be20b07886ed01a
KJ <kangjing324@live.com>
2021-04-08 19:13:19 +0800

    From KJ

--- app/src/main/java/com/example/kcttwarehousemobilesystem/MainActivity.kt
+++ app/src/main/java/com/example/kcttwarehousemobilesystem/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 //lol
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/kcttwarehousemobilesystem/RegisterMaterial.kt
+++ app/src/main/java/com/example/kcttwarehousemobilesystem/RegisterMaterial.kt
@@ -3,9 +3,9 @@ package com.example.kcttwarehousemobilesystem
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class Materials : AppCompatActivity() {
+class RegisterMaterial : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_materials)
+        setContentView(R.layout.activity_register_material)
     }
 }
