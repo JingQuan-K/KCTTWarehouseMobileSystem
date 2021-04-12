934f71c6ec307483676b9ae3aade38e6c8ad72e7
KJ <kangjing324@live.com>
2021-04-13 02:30:05 +0800

    From KJ

--- app/src/main/java/com/example/kcttwarehousemobilesystem/MainActivity.kt
+++ app/src/main/java/com/example/kcttwarehousemobilesystem/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.kcttwarehousemobilesystem
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 //hi this is a comment by kjq
 //lol
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_register_material)
+
+        val btn = findViewById<Button>(R.id.button)
+            btn.setOnClickListener {
+            intent = Intent(this, com.example.kcttwarehousemobilesystem.RegisterMaterial::class.java)
+            startActivity(intent)
+        }
     }
 }
