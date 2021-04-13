54fa10daf5bbe0a1cd43fea396ab007713ec25f6
HENRYTZL <henrytzl2@hotmail.com>
2021-04-13 18:08:52 +0800

    Register, Login, Forget Password, Forget Password Continue, Reset Password, Homepage

--- app/src/main/java/com/example/kcttwarehousemobilesystem/MainActivity.kt
+++ app/src/main/java/com/example/kcttwarehousemobilesystem/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.kcttwarehousemobilesystem
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
-
+import kotlinx.android.synthetic.main.login.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,14 +13,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.register)
-        //setContentView(R.layout.login)
+        setContentView(R.layout.login)
         //setContentView(R.layout.forget_password)
         //setContentView(R.layout.forget_password_cont)
         //setContentView(R.layout.reset_password)
-        setContentView(R.layout.homepage)
+        //setContentView(R.layout.homepage)
 
         //toolbar=findViewById<Toolbar>(R.id.myToolBar)
 
         //setSupportActionBar(findViewById(R.id.myToolBar))
+
+        //buttonLogin.setOnClickListener {
+
+        //}
+        btnLogin.setOnClickListener{
+            val intent = Intent(this, Homepage::class.java)
+            startActivity(intent)
+        }
+
     }
 }
