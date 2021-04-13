72945b66e1b8bdce83ccf1a827d7a0224449f631
HENRYTZL <henrytzl2@hotmail.com>
2021-04-14 04:49:49 +0800

    Register, Login, Forget Password, Forget Password Continue, Reset Password, Homepage

--- app/src/main/java/com/example/kcttwarehousemobilesystem/MainActivity.kt
+++ app/src/main/java/com/example/kcttwarehousemobilesystem/MainActivity.kt
@@ -22,8 +22,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.register)
-        setContentView(R.layout.login)
+        setContentView(R.layout.register)
+        //setContentView(R.layout.login)
         //setContentView(R.layout.forget_password)
         //setContentView(R.layout.forget_password_cont)
         //setContentView(R.layout.reset_password)
