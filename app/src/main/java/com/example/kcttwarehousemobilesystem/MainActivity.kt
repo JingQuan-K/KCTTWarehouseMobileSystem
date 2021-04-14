b98fe470bf8d7de46f4d8c7b8fac17451840d8df
HENRYTZL <henrytzl2@hotmail.com>
2021-04-14 23:51:56 +0800

    register page btn done linking

--- app/src/main/java/com/example/kcttwarehousemobilesystem/MainActivity.kt
+++ app/src/main/java/com/example/kcttwarehousemobilesystem/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,8 +17,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.register)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
+        //setContentView(R.layout.activity_login)
         //setContentView(R.layout.forget_password)
         //setContentView(R.layout.forget_password_cont)
         //setContentView(R.layout.reset_password)
@@ -26,23 +27,38 @@ class MainActivity : AppCompatActivity() {
 
         //Login page
         //Login button
-        btnLogin.setOnClickListener {
+/*        btnLogin.setOnClickListener {
             val intent = Intent(this, Homepage::class.java)
             startActivity(intent)
         }
 
+        //Sign Up text button
         textBtnSignUp.setOnClickListener {
             val intent = Intent(this, Register::class.java)
             startActivity(intent)
         }
 
+        //Forget pass text button
         textBtnForgotPass.setOnClickListener {
             val intent = Intent(this, Forget_password::class.java)
+            startActivity(intent)
+        }*/
+
+        //Register page
+        //Sign Up button
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        //Sign In text button
+        textBtnSignIn.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
 
 
-
+        //Homepage
         /*toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
