4bbc39965e625ca8ae7aef847f66f868cdca141c
HENRYTZL <henrytzl2@hotmail.com>
2021-04-14 22:22:37 +0800

    reset page link to home page

--- app/src/main/java/com/example/kcttwarehousemobilesystem/fragments/reset/Reset.kt
+++ app/src/main/java/com/example/kcttwarehousemobilesystem/fragments/reset/Reset.kt
@@ -5,12 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.example.kcttwarehousemobilesystem.R
 
-class Reset : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reset, container, false)
+class Reset : AppCompatActivity() {
+    override fun onCreate (savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_reset)
     }
 }
