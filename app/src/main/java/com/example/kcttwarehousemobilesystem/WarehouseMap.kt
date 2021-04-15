9045675f4e9ef9b7d2a1972505a140c1aee3b4cc
JingQuan-K <jingquan127@gmail.com>
2021-04-15 17:29:31 +0800

    commit by kjq

--- app/src/main/java/com/example/kcttwarehousemobilesystem/WarehouseMap.kt
+++ app/src/main/java/com/example/kcttwarehousemobilesystem/WarehouseMap.kt
@@ -1,19 +1,39 @@
 package com.example.kcttwarehousemobilesystem
 
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_warehouse_map.*
 
 class WarehouseMap : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_warehouse_map)
 
-        //Action Bar
-        val actionBar = supportActionBar
-        actionBar!!.title = "Warehouse Map"
-        actionBar.setDisplayHomeAsUpEnabled(true)
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
 
+        val search = menu?.findItem(R.id.menu_search)
+        val searchView = search?.actionView as SearchView
+        searchView.queryHint = "Search material"
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                A_01a_01.setImageResource(R.drawable.square_found)
+                return true
+            }
+
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
 }
