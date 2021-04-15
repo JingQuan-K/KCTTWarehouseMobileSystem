fa284b4ac6edde755949f5e15dd4831d6ae67e61
tengsiang <chents-pm18@student.tarc.edu.my>
2021-04-15 20:01:15 +0800

    open view model and report

--- app/src/main/java/com/example/kcttwarehousemobilesystem/Report.kt
+++ app/src/main/java/com/example/kcttwarehousemobilesystem/Report.kt
@@ -1,13 +1,20 @@
 package com.example.kcttwarehousemobilesystem
 
 import android.os.Bundle
+import android.widget.ListAdapter
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kcttwarehousemobilesystem.MaterialFragment.Report
+import com.example.kcttwarehousemobilesystem.materialType.MaterialVM
 
 class Report : AppCompatActivity() {
+
+    private lateinit var mMaterialVM: MaterialVM
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.report)
+
+        val adapter = Report()
     }
 
 
