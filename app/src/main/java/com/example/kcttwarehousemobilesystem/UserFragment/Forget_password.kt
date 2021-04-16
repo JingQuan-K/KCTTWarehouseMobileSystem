92dec3b18df4ec24e62732733816d473ad6cdd43
HENRYTZL <henrytzl2@hotmail.com>
2021-04-17 01:32:50 +0800

    linking

--- app/src/main/java/com/example/kcttwarehousemobilesystem/UserFragment/Forget_password.kt
+++ app/src/main/java/com/example/kcttwarehousemobilesystem/UserFragment/Forget_password.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.kcttwarehousemobilesystem.R
+import kotlinx.android.synthetic.main.fragment_forget_password.*
+import kotlinx.android.synthetic.main.fragment_forget_password.view.*
 
 class Forget_password : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forget_password, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_forget_password, container, false)
 
+        view.btnSendFrag.setOnClickListener {
+            findNavController().navigate(R.id.action_ForgetPasswordPage_to_forget_password_cont)
+        }
+        return view
+    }
 }
